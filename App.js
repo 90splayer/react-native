61372375a1e2061e45302d32e3d56f45096aab30
Michael Ojogbo <linkupxc@gmail.com>
2024-02-25 23:48:12 +0100

    form

--- App.js
+++ App.js
@@ -1,62 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { ActivityIndicator, Button, Image, ImageBackground, Modal, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
-import Greet from './components/Greet';
 
-const logoImg = require("./assets/adaptive-icon.png")
+import {  StyleSheet, SafeAreaView, Platform, ScrollView } from 'react-native';
+import PokemonCard from './components/PokemonCard';
+import pokemonList from './data.json'
 
 export default function App() {
 
-  const [isModalVisible, setIsModalVisible] = useState(false)
+  const charmanderData = {
+    name: "Charmander",
+    image: require("./assets/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  };
+
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"), // Replace with the actual image path
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("./assets/pikachu.png"), // Replace with the actual image path
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  };
 
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60}}>
-    <ScrollView>
-    <StatusBar backgroundColor='lightgreen' barStyle="light-content"/>
-    <Image source={logoImg} style={{width: 300, height: 300}}/>
-    <Button title='Press' onPress={()=> setIsModalVisible(true)}
-      color="midnightblue"
-      disabled={isModalVisible}
-    />
-    <Greet name="Bruce Wayne"/>
-    <Button title='Alert' onPress={() => Alert.alert("Invalid data!", "DOB incorrect", [
-      {
-        text: "Cancel",
-        onPress: () => console.log("Cancel pressed")
-      },
-      {
-        text: "OK",
-        onPress: () => console.log("OK pressed")
-      }
-    ])} />
-    <ActivityIndicator size="large" color="midnightblue" animating={false} />
-    <Pressable onPress={() => console.log("image pressed")}>
-    <Image source={logoImg} style={{width: 300, height: 300}}/>
-    </Pressable>
-    <Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)}
-    animationType='slide' presentationStyle='pageSheet'>
-      <View style={styles.container}>
-        <Text>Modal content</Text>
-        <Button 
-        title='Close' 
-        color="midnightblue"
-        onPress={() => setIsModalVisible(false)}
-        />
-      </View>
-    </Modal>
-    {/* <Image 
-      source={{ url: "https://picsve.photos/300"}}
-      style={{width: 300, height: 300}}
-    /> */}
-    {/* <ImageBackground source={logoImg} style={{flex: 1}}>
-    <Text>Awesome feature sharp!</Text>
-    </ImageBackground>
-      <StatusBar style="auto" /> */}
-    </ScrollView>
-    </View>
+    <SafeAreaView style={styles.container}>
+     <ScrollView>
+        <PokemonCard {...charmanderData} />
+        <PokemonCard {...squirtleData} />
+        <PokemonCard {...bulbasaurData} />
+        <PokemonCard {...pikachuData} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "lightblue", padding: 60}
+  container: { 
+    flex: 1, 
+    backgroundColor: "f5f5f5",
+    paddingTop: Platform.OS === 'android' ? 25 : 0 
+  }
 })
